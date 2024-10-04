246d1990c73824766361656af876a4a407062b7c
tungduong <duongday69@gmail.com>
2024-10-04 08:20:38 +0700

    first

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+#include "LED.h"
+#include <OneButton.h>
+
+// Giả sử LED_PIN, LED_ACT, BTN_PIN, BTN_ACT đã được định nghĩa ở nơi khác
+LED led(LED_PIN, LED_ACT);
+
+void btnPush();
+void btnDoubleClick(); // Hàm mới cho double click
+OneButton button(BTN_PIN, !BTN_ACT);
+
+void setup()
+{
+    led.off();
+    button.attachClick(btnPush);
+    button.attachDoubleClick(btnDoubleClick); // Liên kết double click
+}
+
+void loop()
+{
+    led.loop();
+    button.tick();
+}
+
+void btnPush()
+{
+    led.flip();
+}
+
+void btnDoubleClick()
+{
+    led.blink(200); // Bắt đầu nhấp nháy LED
+}
